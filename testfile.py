2572a7a409d087b3ceb05e41c8ab481580c7b815
Spanarchian <itestedthis1@gmail.com>
2016-02-19 09:12:52 +0000

    Fix Failing test - test_1Neg_numbers_3_4
    test_1Neg_numbers_3_4 was ==12 should have been -12

--- testfile.py
+++ testfile.py
@@ -8,7 +8,7 @@ def test_strings_a_3():
     assert multiply('a',3) == 'aaa' 
  
 def test_1Neg_numbers_3_4():
-    assert multiply(-3,4) == 12 
+    assert multiply(-3,4) == -12 
     
 def test_BothNeg_numbers_3_4():
     assert multiply(-3,-4) == 12 
