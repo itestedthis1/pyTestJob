2bb12264041f0e99a0c571efe107332e610bb846
Spanarchian <itestedthis1@gmail.com>
2016-02-21 18:55:26 +0000

    Update pyTestEnv.sh

--- pyTestEnv.sh
+++ pyTestEnv.sh
@@ -5,7 +5,7 @@ fi
 pip install -r requirements.txt 
 
 
-py.test --junitxml=reports/result.xml --html=report.html testfile.py
+py.test --junitxml=reports/result.xml --html=reports/report.html testfile.py
 
 pep8 calc.py > pep8.log || true
 pylint calc.py > pylint.log || true
