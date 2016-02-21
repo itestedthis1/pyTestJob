e3d2d83f05cb40607fcd263313fa4370fdf6d484
Colin Moore-Hill <colin.moore-hill@met.police.uk>
2016-02-21 12:26:28 +0000

    modified:   pyTestEnv.sh

--- pyTestEnv.sh
+++ pyTestEnv.sh
@@ -4,6 +4,10 @@ fi
 . pyEnvJenkins/bin/activate
 pip install -r requirements.txt 
 
-py.test --html=report.html testfile.py
+
+py.test --junitxml=/report --html=report.html testfile.py
+
+pep8 calc.py > pep8.log || true
+pyflakes calc.py > pyflakes.log || true
 
 deactivate
